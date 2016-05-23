f3900d04b6dda6c35feb4f01ae1217dcebb56932
lycoris102 <lycoris102@gmail.com>
2016-05-23 14:38:26 +0900

    add Project

---
+++ Assets/Scripts/Touch.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Touch : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject explosionPrefab;
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector2 touchPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            GameObject explosion = (GameObject)Instantiate(explosionPrefab, touchPosition, Quaternion.identity);
+            Destroy(explosion, 1.5f);
+        }
+    }
+}
